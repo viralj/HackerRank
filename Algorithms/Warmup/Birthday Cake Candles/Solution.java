93dae375a637f714c973838604ce31df2df6bcfe
Viral Joshi <viralj@users.noreply.github.com>
2017-09-25 16:30:08 -0500

    Create Solution.java

---
+++ Algorithms/Warmup/Birthday Cake Candles/Solution.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int birthdayCakeCandles(int n, int[] ar) {
+        // Complete this function
+        
+        int q =0, w=0;
+        
+        for (int o : ar){
+            if(q<o ){
+                q=o;
+                w =1;
+            }else if(q==o){
+                w++;
+            }
+        }
+        
+        return w;
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = birthdayCakeCandles(n, ar);
+        System.out.println(result);
+    }
+}
